c9fdac2b474cc8ffde9d3fdb251eeb26a14ba059
oyis12 <oyivosam12@gmail.com>
2024-07-09 07:38:28 +0100

    services banner added

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="w-full h-[300px] bg-[url('assets/image/bg_gallery.jpg')] relative lg:-top-[4.5rem] bg-cover bg-no-repeat bg-center bg-fixed">
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3'></div>
+    </div>
   )
 }
 
